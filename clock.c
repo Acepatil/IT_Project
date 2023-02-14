c82e65e6468b0278d9dadce9b57ad808c1f69339
Shiv <shivchavda11@gmail.com>
2023-02-14 21:25:11 +0530

    project

---
+++ clock.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+<<<<<<< HEAD
+#include "delay.h"
+// World clock function calling.....
+=======
+// World clock library calling.....
+>>>>>>> c2cdea50005bc3b9ead001d443cf87bfd02222b5
+// selecting time zone and then starting the clock..
+int main()
+{
+    int h=0,m=0,s=0,ms=0,t=1;
+    while(t==1)
+        {
+            printf("Enter Start Hours:");
+            scanf("%d",&h);
+            printf("Enter Start Minute:");
+            scanf("%d",&m);
+            printf("Enter Start Seconds:");
+            scanf("%d",&s);
+            t--;
+        }
+    while (1)
+    {
+        delay(1);
+        system("cls");
+        s++;
+        if (s==60)
+        {
+            s=0;
+            m=m+1;
+        }
+        
+        if (m==60)
+        {
+            m=0;
+            h=h+1;
+        }
+        printf("Time :  ");  
+        printf("%d : %d : %d ",h,m,s);
+    }
+}
